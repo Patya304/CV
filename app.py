d8d15c1576127c175defefb1ce6a07402cb379e4
md_patrik <xd2.0.hun@gmail.com>
2024-02-01 14:28:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,7 @@ from pathlib import Path
 
 import streamlit as st
 from PIL import Image
-# --- LOAD CSS, PDF & PROFIL PIC ---
-with open(css_file) as f:
-    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
-with open(resume_file, "rb") as pdf_file:
-    PDFbyte = pdf_file.read()
-profile_pic = Image.open(profile_pic)
+
 
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
@@ -32,6 +27,12 @@ SOCIAL_MEDIA = {
 
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
+# --- LOAD CSS, PDF & PROFIL PIC ---
+with open(css_file) as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+with open(resume_file, "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+profile_pic = Image.open(profile_pic)
 
 # --- HERO SECTION ---
 col1, col2 = st.columns(2, gap="small")
